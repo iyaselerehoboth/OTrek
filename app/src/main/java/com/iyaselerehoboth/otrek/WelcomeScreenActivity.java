2080f160977a0769cd2d239c07d3f15c0e0c333b
Rehoboth <rehobothi@yahoo.com>
2020-02-09 20:53:09 +0100

    Developed facebook login to a measure

--- app/src/main/java/com/iyaselerehoboth/otrek/WelcomeScreenActivity.java
+++ app/src/main/java/com/iyaselerehoboth/otrek/WelcomeScreenActivity.java
@@ -1,13 +1,32 @@
 package com.iyaselerehoboth.otrek;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,7 +34,7 @@ import butterknife.OnClick;
 
 public class WelcomeScreenActivity extends AppCompatActivity {
     @BindView(R.id.btn_facebook_login)
-    MaterialButton btn_facebook_login;
+    LoginButton btn_facebook_login;
 
     @BindView(R.id.btn_twitter_login)
     MaterialButton btn_twitter_login;
@@ -25,6 +44,10 @@ public class WelcomeScreenActivity extends AppCompatActivity {
 
     @BindView(R.id.btn_google_login)
     MaterialButton btn_google_login;
+
+    CallbackManager mCallbackManager;
+    private FirebaseAuth mAuth;
+    private static String TAG = "OTrek CHECK";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +59,77 @@ public class WelcomeScreenActivity extends AppCompatActivity {
         // To make the notification bar transparent.
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        /*Facebook's app events logger*//*
+        FacebookSdk.sdkInitialize(WelcomeScreenActivity.this);
+        AppEventsLogger.activateApp(this);*/
+
+        mCallbackManager = CallbackManager.Factory.create();
+        btn_facebook_login.setReadPermissions("email", "public_profile");
+        btn_facebook_login.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //Do Something with it.
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @OnClick(R.id.btn_email_login)
     public void emailLogin(){
         startActivity(new Intent(WelcomeScreenActivity.this, ProfilePageActivity.class));
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d("OTrek CHECK", "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithCredential:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Log.d(TAG, user.getDisplayName() + " " + user.getPhoneNumber() + " " + user.getPhotoUrl());
+                        Toast.makeText(this, user.getPhoneNumber(), Toast.LENGTH_LONG).show();
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithCredential:failure", task.getException());
+                        Toast.makeText(WelcomeScreenActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                    // ...
+                });
     }
 
 
