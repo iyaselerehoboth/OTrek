606c4ab363a6537833794a91df332e695b462464
Rehoboth <rehobothi@yahoo.com>
2020-03-16 13:17:29 +0100

    Switching the app to Kotlin.

---
+++ app/src/main/java/com/iyaselerehoboth/otrek/WelcomeScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.iyaselerehoboth.otrek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_screen)
+    }
+}
